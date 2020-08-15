95be1706252a9afd67ce3740e11c5bef604ba61a
ToroChu <whileonrails@gmail.com>
2020-08-15 14:32:52 +0800

    47-48 xmlhttprequest

---
+++ JSnewbie/47-48/XMLHttpRequest/index.js
@@ -0,0 +1,62 @@
+
+// // XMLHttpRequest
+// let xhr = new XMLHttpRequest();
+// xhr.open('POST', 'https://hexschool-tutorial.herokuapp.com/api/signup', true);
+
+// // 格式：application/x-www-form-urlencoded
+// xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+// xhr.send('email=abcde@gmail.com');
+
+// // 格式：JSON
+// // let account = {
+// //   email:'abcdef@gmail.com',
+// //   password: '1234'
+// // };
+// // xhr.setRequestHeader("Content-type", "application/json");
+// // xhr.send(JSON.stringify(account));
+
+const wrap = document.querySelector('.wrap');
+const loginBtn = document.querySelector('#login-btn');
+const loginAPI = 'https://hexschool-tutorial.herokuapp.com/api/signin';
+
+let login = (e) => {
+  e.preventDefault();
+  let email = document.querySelector('input[name="email"]').value;
+  let password = document.querySelector('input[name="password"]').value;
+  let accInfo = {};
+  accInfo.email = email;
+  accInfo.password = password;
+
+  let xhr = new XMLHttpRequest();  
+  xhr.open('POST', loginAPI, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  console.log(JSON.stringify(accInfo));
+  xhr.send(JSON.stringify(accInfo));
+
+  xhr.onload = () => {
+    let callbackData = JSON.parse(xhr.responseText);
+    console.log(callbackData);
+    let verify = callbackData.message;
+    if (verify === "登入成功"){
+      const wrap = document.querySelector('.wrap');
+      wrap.classList.add('fade');
+      
+      setTimeout( () => {
+        wrap.classList.add('fade-out');
+        wrap.classList.remove('fade');
+        wrap.innerHTML = '';
+      }, 2500);
+
+      setTimeout( () => {
+      window.location = '.';
+      }, 3000);
+    } else {
+      const helpText = document.querySelector('#helpText');
+      helpText.textContent = verify;
+    }
+  }
+}
+
+loginBtn.addEventListener('click', login);
+
+
