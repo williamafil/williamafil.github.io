013f08aa77dc72a004efdc0220daac5fdae64220
ToroChu <whileonrails@gmail.com>
2020-08-26 02:21:10 +0800

    remove comments and spaces

--- JS/SimpleDatePicker/simple-date-picker/simple-date-picker.js
+++ JS/SimpleDatePicker/simple-date-picker/simple-date-picker.js
@@ -31,34 +31,28 @@ const datePicker = ({root}) => {
   const prevBtn = root.querySelector('.simple-date-picker .dates .month .prev-month');
   const nextBtn = root.querySelector('.simple-date-picker .dates .month .next-month');
   const daysElement = root.querySelector('.simple-date-picker .dates .days');
-  const weeksElement = root.querySelector('.simple-date-picker .dates .weeks');
-
 
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-  // 當下日期
   let date = new Date();
   let day = date.getDate();
   let month = date.getMonth();
   let year = date.getFullYear();
 
-  // 選擇的日期
   let selectedDate = date;
   let selectedDay = day;
   let selectedMonth = month;
   let selectedYear = year;
 
-  monthElement.textContent = `${ month+1 }月  ${year}年`;  // 展開時顯示的月份＆年份
+  monthElement.textContent = `${ month+1 }月  ${year}年`;
   selectedDateElement.textContent = formatDate(date);
   populateDates(month, year);
 
   datepickerElement.addEventListener('click', toggleDatepicker);
-  prevBtn.addEventListener('click', goPrevMonth); // 往前一個月事件監聽
-  nextBtn.addEventListener('click', goNextMonth); // 往後一個月事件監聽
+  prevBtn.addEventListener('click', goPrevMonth);
+  nextBtn.addEventListener('click', goNextMonth);
   
   document.addEventListener('click', function(e){
-    console.log('root: ', root);
-    console.log('e.target', e.target)
     if (!root.contains(e.target)){
       datesElement.classList.remove('active');
     }
@@ -93,26 +87,20 @@ const datePicker = ({root}) => {
   function populateDates(mth, yr) {
     daysElement.innerHTML = '';
     
-    let amountDays = getTotalDays(mth, yr); // 取得月份共幾天
+    let amountDays = getTotalDays(mth, yr);
     let firstDay = getStartedWeekDay(mth, yr);
     let spacer = ``;
 
     for (let i = 0; i < firstDay; i++) {
       spacer += `<div class="space"></div>`;
     }
-    console.log(spacer);
     daysElement.innerHTML = spacer;
-
-    console.log(mth);
-    console.log(yr);
-    console.log(firstDay);
 
     for (let i = 0; i < amountDays; i++) {
       const dayElement = document.createElement('div');
       dayElement.classList.add('day');
       dayElement.textContent = i + 1;
   
-      // 遇到當下日期/被選擇的日期，加上 css style
       if (selectedDay == (i + 1) && selectedYear == year && selectedMonth == month) {
         dayElement.classList.add('selected');
       }
@@ -124,9 +112,9 @@ const datePicker = ({root}) => {
         selectedYear = year;
   
         selectedDateElement.textContent = formatDate(selectedDate);
-        selectedDateElement.dataset.value = dataSetFormat(selectedDate); // 在元素上加入 dataset，之後要取得日期比較容易。
+        selectedDateElement.dataset.value = dataSetFormat(selectedDate);
 
-        populateDates(selectedMonth, selectedYear); // 當選擇了某一天，日期區塊需要重新更新。
+        populateDates(selectedMonth, selectedYear);
        });
 
      daysElement.appendChild(dayElement);
@@ -171,6 +159,4 @@ const datePicker = ({root}) => {
   function getStartedWeekDay(month, year) {
     return new Date(year, month, 1).getDay();
   }
-
-
 }
